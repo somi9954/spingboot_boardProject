3cf0a96c8e5f132c0624616e34984c5d017485d4
somi9954 <jolh999@gmail.com>
2023-10-29 18:39:13 +0900

    :tada: 초기설정

---
+++ src/test/java/boardproject/SpingbootBoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package boardproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingbootBoardProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
