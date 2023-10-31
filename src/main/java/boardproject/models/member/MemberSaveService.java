3b936238449e6d94ca41f14f5ae1133d460a195f
somi9954 <jolh999@gmail.com>
2023-11-01 00:58:51 +0900

    회원 저장, 수정 서비스 개발 중

---
+++ src/main/java/boardproject/models/member/MemberSaveService.java
@@ -0,0 +1,11 @@
+package boardproject.models.member;
+
+import org.springframework.stereotype.Service;
+
+/**
+ *  회원 정보 추가, 수정
+ *   - 수정시 비밀번호는 값이 있을때만  수정
+ */
+@Service
+public class MemberSaveService {
+}
---
+++ src/main/java/boardproject/repositories/MemberRepository.java
@@ -0,0 +1,9 @@
+package boardproject.repositories;
+
+import boardproject.entities.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+public interface MemberRepository  extends JpaRepository<Member, Long>, QuerydslPredicateExecutor<Member> {
+
+}
