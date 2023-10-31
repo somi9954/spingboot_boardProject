bc7a30459aff81193c11f944208ba091d5cc321a
somi9954 <jolh999@gmail.com>
2023-11-01 01:47:47 +0900

    회원 가입 유효성 검사

--- src/main/java/boardproject/configs/MvcConfig.java
+++ src/main/java/boardproject/configs/MvcConfig.java
@@ -3,12 +3,18 @@ package boardproject.configs;
 import boardproject.configs.interceptors.SiteConfigInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ResourceBundle;
 
 @Configuration
 @RequiredArgsConstructor
@@ -36,5 +42,18 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(siteConfigInterceptor)
                 .addPathPatterns("/**");
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
+        ms.setDefaultEncoding("UTF-8");
+        ms.setBasenames("messages.commons", "messages.validations", "messages.errors" );
+
+        return ms;
+    }
+    @Bean
+    public HiddenHttpMethodFilter httpMethodFilter() { //GET, POST외에 DELETE, PATCH, PUT... 추가
+        return new HiddenHttpMethodFilter();
     }
 }
