41cd4d93ac4b11fa6684792cf4341051aed15040
somi9954 <jolh999@gmail.com>
2023-11-02 01:30:04 +0900

    회원 가입 유효성 검사

--- src/main/java/boardproject/controllers/members/JoinForm.java
+++ src/main/java/boardproject/controllers/members/JoinForm.java
@@ -1,5 +1,9 @@
 package boardproject.controllers.members;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,12 +12,23 @@ import lombok.NoArgsConstructor;
 @Data @Builder
 @NoArgsConstructor @AllArgsConstructor
 public class JoinForm {
-
+    @NotBlank
+    @Size(min=6, max = 20)
     private String userId; // 아이디
+
+    @NotBlank
+    @Size(min = 8)
     private String userPw;
+
+    @NotBlank
     private String userPwRe;
+
+    @NotBlank
     private String userNm;
+
+    @NotBlank @Email
     private String email;
     private String mobile;
+
     private boolean[] agrees;
 }
