db6acc2a35624709357d0f9651ced6c49512b12a
somi9954 <jolh999@gmail.com>
2023-11-04 23:05:33 +0900

    비밀번호 복잡성 오류 수정

--- src/main/java/boardproject/controllers/members/JoinValidator.java
+++ src/main/java/boardproject/controllers/members/JoinValidator.java
@@ -41,12 +41,14 @@ public class JoinValidator implements Validator, MobileValidator, PasswordValida
             errors.rejectValue("userId", "Validation.duplicate.userId");
         }
 
-      /*  // 2. 비밀번호 복잡성 체크(알파벳(대문자, 소문자), 숫자, 특수문자))
+      // 2. 비밀번호 복잡성 체크(알파벳(대문자, 소문자), 숫자, 특수문자))
         if (userPw != null && !userPw.isBlank()
-        && (!alphaCheck(userPw, false) || numberCheck(userPw) || specialCharsCheck(userPw))) {
-            errors.rejectValue("userPw", "Validation.complexity.password");
-        }*/
+                && (!alphaCheck(userPw, false)
+                || !numberCheck(userPw)
+                || !specialCharsCheck(userPw))) {
 
+            errors.rejectValue("userPw", "Validation.complexity.password");
+        }
 
 
         //3. 비밀번호와 비밀번호 확인 일치
