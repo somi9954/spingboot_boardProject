25264566528f637a65b54b8d70f9ec15761da8c3
somi9954 <jolh999@gmail.com>
2023-11-04 23:10:02 +0900

    비밀번호 복잡성 오류 수정

--- src/main/java/boardproject/commons/validators/PasswordValidator.java
+++ src/main/java/boardproject/commons/validators/PasswordValidator.java
@@ -1,23 +1,21 @@
-package boardproject.commons.validators;
+package org.koreait.commons.validators;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * The interface Password validator.
- */
 public interface PasswordValidator {
 
     /**
      * 비밀번호 복잡성 체크 - 알파벳 체크
+     *
      * @param password
      * @param caseIncentive
-     *        false : 소문자 + 대문자가 반드시 포함되는 패턴
-     *        true : 대소문자 상관없이 포함되는 패턴
+     *          false : 소문자 + 대문자가 반드시 포함되는 패턴
+     *          true : 대소문자 상관없이 포함되는 패턴
      * @return
      */
-    default boolean alphaCheck(String password, boolean caseIncentive){
-        if (caseIncentive) { // 대소문자 구분 없이 체크
+    default boolean alphaCheck(String password, boolean caseIncentive) {
+        if (caseIncentive) { // 대소문자 구분없이 체크
             Pattern pattern = Pattern.compile("[a-z]+", Pattern.CASE_INSENSITIVE);
             return pattern.matcher(password).find();
         }
@@ -27,9 +25,9 @@ public interface PasswordValidator {
         Pattern pattern2 = Pattern.compile("[A-Z]+");
         return pattern1.matcher(password).find() && pattern2.matcher(password).find();
     }
-
     /**
      * 숫자가 포함된 패턴인지 체크
+     *
      * @param password
      * @return
      */
@@ -40,7 +38,7 @@ public interface PasswordValidator {
     }
 
     /**
-     * 특수문자가 포함된 문자인지 체크
+     * 특수문자가 포함된 패턴인지 체크
      * @param password
      * @return
      */
