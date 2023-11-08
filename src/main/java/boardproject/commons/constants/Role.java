8f462f00dc383863d72c6446094f9ada5505b3ae
somi9954 <jolh999@gmail.com>
2023-11-08 23:37:14 +0900

    re

--- src/main/java/boardproject/commons/constants/Role.java
+++
@@ -1,6 +0,0 @@
-package boardproject.commons.constants;
-
-public enum Role {
-    USER, // 일반 사용자
-    ADMIN // 관리자
-}
--- src/main/java/boardproject/restcontrollers/CommonRestController.java
+++
@@ -1,7 +0,0 @@
-package boardproject.restcontrollers;
-
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice("boardproject.controllers")
-public class CommonRestController {
-}
