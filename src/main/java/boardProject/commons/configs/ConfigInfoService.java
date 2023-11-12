ba488357ab0872acd0dcee0029bb134896c62c69
somi9954 <jolh999@gmail.com>
2023-11-12 18:24:13 +0900

    게시판 설정 작업 중

--- src/main/java/boardProject/commons/configs/ConfigInfoService.java
+++ src/main/java/boardProject/commons/configs/ConfigInfoService.java
@@ -3,6 +3,7 @@ package boardProject.commons.configs;
 import boardProject.entities.Configs;
 import boardProject.repositories.ConfigsRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,14 @@ public class ConfigInfoService {
     private final ConfigsRepository repository;
 
     public <T> T get(String code, Class<T> clazz) {
+        return  get(code, clazz, null);
+    }
+
+    public <T> T get(String code, TypeReference<T> type) {
+        return get(code, null, type);
+    }
+
+    public <T> T get(String code, Class<T> clazz, TypeReference<T> typeReference) {
         Configs configs = repository.findById(code).orElse(null);
         if (configs == null || configs.getValue() == null || configs.getValue().isBlank()) {
             return null;
@@ -24,7 +33,10 @@ public class ConfigInfoService {
         ObjectMapper om = new ObjectMapper();
         T data = null;
         try {
-            data = om.readValue(value, clazz);
+
+            if (clazz == null)  data = om.readValue(value, typeReference);
+            else data = om.readValue(value, clazz);
+
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
--- src/main/java/boardProject/controllers/admins/ConfigController.java
+++ src/main/java/boardProject/controllers/admins/ConfigController.java
@@ -1,25 +1,40 @@
 package boardProject.controllers.admins;
 
+import boardProject.commons.configs.ConfigInfoService;
+import boardProject.commons.configs.ConfigSaveService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@Log
 @Controller
 @RequestMapping("/admin/config")
+@RequiredArgsConstructor
 public class ConfigController {
+
+    private final ConfigSaveService saveService;
+    private final ConfigInfoService infoService;
+    private String code = "siteConfig";
 
     @GetMapping
     public String config(Model model) {
         commonProcess(model);
+        ConfigForm configForm = infoService.get(code, ConfigForm.class);
 
+        model.addAttribute("configForm", configForm == null ? new ConfigForm() : configForm);
         return "admin/config";
     }
 
     @PostMapping
-    public String configPs( Model model) {
+    public String configPs(ConfigForm configForm, Model model) {
         commonProcess(model);
+
+        saveService.save(code, configForm);
+
+        model.addAttribute("message", "설정이 저장되었습니다.");
 
         return "admin/config";
     }
